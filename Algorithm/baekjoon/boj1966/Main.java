a7b6f3a89251254c4a2e72da34aecfb6afcaef55
Ju-hyunLee <juhyun903@naver.com>
2018-01-16 21:29:07 +0900

    18. 1. 16. tue. 다시 시도할거

--- Algorithm/baekjoon/boj1966/Main.java
+++ Algorithm/baekjoon/boj1966/Main.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 //우선순위 큐 사용해서 다시 풀어보기 
-public class Main {
+public class Main { 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = Integer.parseInt(sc.nextLine()); //시행 횟수
