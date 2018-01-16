69180b133b5442c23b782a1532a3b5a7f733481e
Ju-hyunLee <juhyun903@naver.com>
2018-01-16 21:30:50 +0900

    18. 1. 16. tue.

--- Algorithm/baekjoon/boj7785/Main.java
+++ Algorithm/baekjoon/boj7785/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		int T = Integer.parseInt(sc.nextLine()); //기록될 출입 기록의 수 
 
 		ArrayList enterList = new ArrayList();
